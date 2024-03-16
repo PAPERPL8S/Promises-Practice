5c2730fb3f1bd4b69993b621afd65dafdbb3e059
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-03-16 16:03:22 -0500

    completed e4.js

--- exercises/e2.js
+++ exercises/e2.js
@@ -42,7 +42,7 @@ export const handlePromise = (promise) => {
       return resolvedValue;
     },
     (rejectedResponse) => {
-      return rejectedResponse;
+      return 'Uh Oh';
     }
   );
 };
